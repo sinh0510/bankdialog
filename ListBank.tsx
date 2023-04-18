608662e6004b04fec184154927bc5c4321d6c679
sinh0510 <109408211+sinh0510@users.noreply.github.com>
2023-04-18 12:20:30 +0700

    Update ListBank.tsx

--- ListBank.tsx
+++ ListBank.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import { BankInner, ListBankInner, ListBankItem, StyleError } from "./styled/UserCreateAutoFillRequestForm";
 import ErrorBoundary from "../../../utils/ErrorBoundary";
 import Bank from "../../../models/Bank";
-import { UseFormRegister, useForm, Controller } from "react-hook-form";
+import { UseFormRegister, useForm, Controller, Control } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { ParamsEmptyForm } from "../../../services/BankService";
+import { ParamsEmptyForm } from "../../../services/WithdrawRequestSevice";
 export type PersonalBank = {
 	id?: number | undefined | null;
 	bankName?: string | undefined | null;
@@ -13,80 +13,33 @@ export type PersonalBank = {
 	bankOwner?: string | undefined | null;
 };
 export default function ListBank(props: {
-	register: UseFormRegister<{
-		bankId:number;
-		currencyId: number;
-		amount: number;
-		bankInfo: string;
-		description: string;
-	}>;
+	register: UseFormRegister<ParamsEmptyForm>;
+	control: Control<ParamsEmptyForm, any>;
 	bankInfo: Bank[];
 }) {
-	const [inputBank, setInputBank] = useState<PersonalBank>({});
-	const schema = yup.object().shape({
-		id: yup.number().positive("Vui lòng chọn ngân hàng").required("Vui lòng chọn ngân hàng"),
-	  });
-	const {
-		handleSubmit,
-		formState: { errors },
-		control,
-	} = useForm<{
-		id: number;
-		bankName: string;
-		bankCardId: string;
-		bankOwner: string;
-	}>({
-		defaultValues: {
-			id: inputBank.id || 0,
-			bankName: inputBank.bankName || "",
-			bankCardId: inputBank.bankCardId || "",
-			bankOwner: inputBank.bankOwner || "",
-		  },
-		resolver: yupResolver(schema),
-	});
-	const onConfirm = async (data: ParamsEmptyForm) => {
-		const selectedBank = props.bankInfo.find((bank) => bank.id === data.id);
-	  
-		if (selectedBank) {
-		  const bankInfo = {
-			id: selectedBank.id || 0,
-			bankName: selectedBank.bankName || '',
-			bankCardId: selectedBank.bankCardId || '',
-			bankOwner: selectedBank.bankOwner || '',
-		  };
-		  
-		  setInputBank(bankInfo);
-
-		}
-	  };
-// 
 	return (
 		<>
-		<Controller
-			control={control}
-			name="id"
-			
-			render={({ field: { onChange, value } }) => (
-				<>
-					{props.bankInfo.map((bank) => {
-						return (
-							<ErrorBoundary key={bank.id}>
-								<ListBankItem
-									selected={value === bank.id}
-									
-									onClick={() => {
-										onChange(bank.id);
-										handleSubmit(onConfirm)();
-									  }}>
-									{bank.bankName} - {bank.bankCardId} - {bank.bankOwner}
-								</ListBankItem>
-							</ErrorBoundary>
-						);
-					})}
-					<StyleError>{errors.id?.message}</StyleError>
-				</>
-			)}
-		/>
+			<Controller
+				control={props.control}
+				name="bankId"
+				render={({ field: { onChange, value } }) => (
+					<>
+						{props.bankInfo.map((bank) => {
+							return (
+								<ErrorBoundary key={bank.id}>
+									<ListBankItem
+										selected={value === bank.id}
+										onClick={() => {
+											onChange(bank.id);
+										}}>
+										{bank.bankName} - {bank.bankCardId} - {bank.bankOwner}
+									</ListBankItem>
+								</ErrorBoundary>
+							);
+						})}
+					</>
+				)}
+			/>
 		</>
 	);
 }
